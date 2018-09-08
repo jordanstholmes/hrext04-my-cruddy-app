f98c0fb3bc80bec985a00cb5e4d236fc78b28953
Jordan Holmes <jordanstholmes@gmail.com>
2018-09-08 09:26:31 -0700

    Playing around with voice recognition

---
+++ mapp.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+  $('.speak').click(startDictation());
+});
+
+
+function startDictation() {
+
+  if (window.hasOwnProperty('webkitSpeechRecognition')) {
+
+    var recognition = new webkitSpeechRecognition();
+
+    recognition.continuous = false;
+    recognition.interimResults = false;
+
+    recognition.lang = "en-US";
+    recognition.start();
+
+    recognition.onresult = function(e) {
+      document.getElementById('transcript').value
+                               = e.results[0][0].transcript;
+      recognition.stop();
+    };
+
+    recognition.onerror = function(e) {
+      recognition.stop();
+    }
+
+  }
+}
